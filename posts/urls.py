b3a00ee0cd0e97b6082ff937fb2c724b25eb8489
Николай <slonopatam52@gmail.com>
2021-01-12 20:37:41 +0300

    2nd review(2.0)

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/<slug>/", views.group_posts, name="group_posts"),
+    path("group/<slug:slug>/", views.group_posts, name="group"),
 ] 
